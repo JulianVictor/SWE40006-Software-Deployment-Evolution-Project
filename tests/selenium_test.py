9ef1223a18e571c170e94db8f026a51798bf6895
Julian Jee Chet Shiang <152185790+JulianVictor@users.noreply.github.com>
2025-05-23 05:02:30 +0800

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Firefox()  # Or Chrome if installed
-driver.get("http://localhost:5000")
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
 
-assert "Cat Facts" in driver.title
-time.sleep(3)
-driver.quit()
+def test_cat_facts_ui():
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://54.91.115.194:5000")
+
+    assert "Cat Facts" in driver.title
+
+    fact = driver.find_element("id", "fact-box").text
+    assert len(fact) > 0
+
+    driver.quit()
